035cf71bfde3466b2328e9802b5963641394fd73
Jonathan Reeve <jon.reeve@gmail.com>
2015-01-30 11:47:02 -0500

    add comments and cleanup to post script

--- scripts/commentpress-post.sh
+++ scripts/commentpress-post.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# This script assists in converting markdown files for the volume
+# Digital Pedagogy into HTML, and them posting them to the CommentPress
+# review site. It may be called like this (BASH):
+
+# for file in hybrid interface praxis queer rhetoric video; do path/to/commentpress-post.sh $file; done
+
 # Get the basename 
 BASENAME=$(basename "$1")
 
@@ -21,3 +29,6 @@ pandoc -o $SHORTNAME.html $1.edited
 
 #Post!
 wp post create $SHORTNAME.html --post_type=page --post_status=publish --post_title="$CAPITALIZED_SHORTNAME" --url=digitalpedagogy.fitzgerald.mlacommons.org 
+
+#Clean up.
+rm $SHORTNAME.html $1.bak $1.edited $1.edited.bak
