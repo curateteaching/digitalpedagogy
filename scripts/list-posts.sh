27d89e74a88b3aa4e14fbd708d2ce9cc1570dc5c
Jonathan Reeve <jon.reeve@gmail.com>
2015-04-14 14:57:19 -0400

    check for server before trying to list posts

--- scripts/list-posts.sh
+++ scripts/list-posts.sh
@@ -1,2 +1,10 @@
-#!/bin/zsh
+# Check to make sure $SERVER is set first. 
+if [ ! -n $SERVER ]
+then 
+	echo 'You must set the environment variable $SERVER for this script to work'
+	exit 1
+fi
+
+echo "Server is: $SERVER" 
+
 wp post list --post_type=page --url=digitalpedagogy.$SERVER
