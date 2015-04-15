6fb16d34d18d8b0ae0a2a623ec4d8c38638276be
Jonathan Reeve <jon.reeve@gmail.com>
2015-04-15 16:27:07 -0400

    better handling of multiple authors

--- scripts/author-extractor.rb
+++ scripts/author-extractor.rb
@@ -1,3 +1,29 @@
 require 'yaml'
 data = YAML::load(STDIN.read)
-puts data['author'][0]['given'] + ' ' + data['author'][0]['family']
+out = ''
+
+# How many authors are we dealing with today? 
+case data['author'].length 
+#If there is one author, don't bother with commas and "and" 
+when 1
+	author = data['author'][0]
+	out += author['given'] + ' ' + author['family']
+# If there are two authors, we can have "and," but not a comma. 
+when 2
+	author = data['author'][0]
+	out += author['given'] + ' ' + author['family'] + ' and '
+	author = data['author'][1]
+	out += author['given'] + ' ' + author['family']
+else
+	# It's list time! 
+	for author in data['author']
+		if author == data['author'].last
+			out += 'and ' + author['given'] + ' ' + author['family']
+		else 
+			# Oxford comma. 
+			out += author['given'] + ' ' + author['family'] + ', '
+		end
+	end
+end
+
+puts out
