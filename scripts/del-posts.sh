f494c71928d29e1da3720a0f14940f08324e6bb1
Jonathan Reeve <jon.reeve@gmail.com>
2015-06-25 13:43:49 -0400

    check for $SERVER in del-posts script

--- scripts/del-posts.sh
+++ scripts/del-posts.sh
@@ -1,2 +1,10 @@
+# Check to make sure $SERVER is set first.
+if [ ! -n "$SERVER" ]
+then
+	echo 'You must set the environment variable $SERVER for this script to work.'
+	echo 'E.g., commons.mla.org, or hardy.dev.'
+	exit 1
+fi
+
 #!/bin/bash
 for id in $@; do wp post delete $id --force --url=digitalpedagogy.$SERVER; done
